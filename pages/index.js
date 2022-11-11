935c79a3a0745fbe39cf6eb4cca62483120ee3e5
feliperomao-dev <feliperomaodev@outlook.com>
2022-11-10 23:06:23 -0300

    atualização aula 3

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,12 @@ import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
-function HomePage() {
-  const estilosDaHomePage = {
-    // backgroundColor: "red"
-  };
+function HomePage() {  
 
   const [valorDoFiltro, setValorDoFiltro] = React.useState(" ");
 
   return (
-    <>
-      <CSSReset />
+    <>     
       <div
         style={{
           display: "flex",
@@ -47,7 +43,9 @@ export default HomePage;
 // }
 
 const StyledHeader = styled.div`
-  .user-info img {
+   background-color: ${({theme}) => theme.backgroundLevel1};
+
+  img {
     width: 80px;
     height: 80px;
     border-radius: 50%;
